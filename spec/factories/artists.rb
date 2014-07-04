9a9b17608b25d029c36d9864da4221d54d1c50fa
Prelang Builder <builder@prelang.com>
2014-07-04 21:37:40 +0000

    Generated model Artist with migration

---
+++ spec/factories/artists.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :artist do
+    name "MyString"
+    description "MyText"
+    began_at "2014-07-04 21:37:40"
+  end
+end
