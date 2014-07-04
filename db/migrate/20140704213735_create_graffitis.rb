aaf332f3240aac6033f0a99596aa87b14963e83c
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-04 18:19:40 -0400

    Change the default state for date time

--- db/migrate/20140704213735_create_graffitis.rb
+++ db/migrate/20140704213735_create_graffitis.rb
@@ -2,10 +2,9 @@ class CreateGraffitis < ActiveRecord::Migration
   def change
     create_table :graffitis do |t|
       t.text :description
-      t.datetime :discovered_at
-      t.datetime :painted_at
-      t.datetime :buffed_at
-
+      t.datetime :discovered_at, :default => Time.now
+      t.datetime :painted_at, :include_blank => true, :default => nil
+      t.datetime :buffed_at, :include_blank => true, :default => nil
       t.timestamps
     end
   end
