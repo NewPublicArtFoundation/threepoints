a5477e7fdae07eb10c79b99bceca07a2c5a8e3a4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-04 22:14:42 -0400

    Change the default route from home to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
