73e9d4483f533ef3361bc72539cc0554a4121664
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-04 20:03:15 -0400

    Change the locations controller to adopt geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,6 @@ class Location < ActiveRecord::Base
   has_and_belongs_to_many :artists
   acts_as_votable
   has_paper_trail
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
