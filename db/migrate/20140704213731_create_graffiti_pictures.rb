51aa2465eb81ce65420d0f67aca4a82dab54eae6
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-06 23:29:16 -0400

    Revert "Remove the graffiti pictures model"
    This reverts commit 872b0f04a158babc4eccf1332e7e3d36a8501aa4.

---
+++ db/migrate/20140704213731_create_graffiti_pictures.rb
@@ -0,0 +1,12 @@
+class CreateGraffitiPictures < ActiveRecord::Migration
+  def change
+    create_table :graffiti_pictures do |t|
+      t.references :graffiti, index: true
+      t.string :image_url
+      t.text :image_description
+      t.references :location, index: true
+
+      t.timestamps
+    end
+  end
+end
