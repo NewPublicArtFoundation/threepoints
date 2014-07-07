471a5e8e6f18a3737b9a70c8e862be243966605c
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-06 23:48:40 -0400

    Resolve the Ransack issue with :users_roles

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register User do
+  remove_filter :users_roles
   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
 
-  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register User do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
