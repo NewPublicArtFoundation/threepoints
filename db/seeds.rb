2cb25acfdaf6dd6dc165f75f8a0a15a244fbb531
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-19 07:31:26 -0400

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create([{
+    name: 'Gift',
+    slug: 'gift',
+    description: 'San Francisco born, international street artist',
+    twitter_url: 'http://twitter.com/rememberlenny',
+    instagram_url: 'http://instagram.com/rememberlenny',
+    flickr_url: 'http://flickr.com/horsegoo',
+    tumblr_url: 'http://blog.rememberlenny.com'
+  },{
+    name: 'Mels',
+    slug: 'mels',
+    description: 'Shanghai based artist from Minneapolis',
+    instagram_url: 'http://instagram.com/rememberlenny',
+    flickr_url: 'http://flickr.com/horsegoo',
+    tumblr_url: 'http://blog.rememberlenny.com'
+  },{
+    name: 'Lame',
+    slug: 'lame',
+    twitter_url: 'http://twitter.com/rememberlenny',
+    instagram_url: 'http://instagram.com/rememberlenny',
+    tumblr_url: 'http://blog.rememberlenny.com'
+  },{
+    name: 'Read',
+    slug: 'read',
+    description: 'Native Shanghainesse',
+    twitter_url: 'http://twitter.com/rememberlenny',
+    instagram_url: 'http://instagram.com/rememberlenny',
+  }
+])
+
+Location.create([{
+    name: 'Moganshan Lu',
+    slug: 'moganshan-lu',
+    address: 'Moganshan lu, Shanghai',
+  },{
+    name: 'Five Points',
+    slug: 'five-points',
+    address: 'Five Points, Queens',
+  },{
+    name: 'Thunder Dome',
+    slug: 'thunder-dome',
+    address: 'Pacific Bell Baseball park',
+  }
+])
+
