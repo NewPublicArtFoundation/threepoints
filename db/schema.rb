f4f79a8770b4f8f2ffcf78686c5e49cc87b25d6f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-24 00:12:19 -0400

    Save the new user id associated to the comments:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724003522) do
+ActiveRecord::Schema.define(version: 20140724041205) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20140724003522) do
     t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "delayed_jobs", force: true do |t|
