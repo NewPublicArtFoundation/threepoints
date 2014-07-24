ace11de361bde971c631748c4866e19924483bd1
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-23 22:40:02 -0400

    Update the activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,15 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.order("created_at desc")
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_activities
+      @activity = Activity.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def activity_params
+      params.require(:activity).permit(:action, :trackable)
+    end
 end
