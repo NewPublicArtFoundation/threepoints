90be7fe374e15e5d397aa32db69a61a38566f02d
RememberLenny <leonard_bogdonoff@condenast.com>
2014-07-23 20:03:05 -0400

    Complete migration

--- app/controllers/graffitis_controller.rb
+++ app/controllers/graffitis_controller.rb
@@ -89,6 +89,6 @@ class GraffitisController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def graffiti_params
-      params.require(:graffiti).permit(:name, :description, :discovered_at, :painted_at, :buffed_at, :location_id, :artist_id, :images)
+      params.require(:graffiti).permit(:name, :slug, :description, :discovered_at, :painted_at, :buffed_at, :location_id, :artist_id, :images)
     end
 end
