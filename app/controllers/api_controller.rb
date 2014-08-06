7c979502d0385c1ba8e8c0c9e9ca55f53fe46f6e
RememberLenny <leonard_bogdonoff@condenast.com>
2014-08-06 09:20:40 -0400

    Create the db save method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,13 @@
 class ApiController < ApplicationController
+  def aws
+    s3 = AWS::S3.new(access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], region: 'us-east-1')
+    bucket = s3.buckets['lkbg-aws-box']
+    bucket.objects.each do |object|
+      url = 'https://s3.amazonaws.com/lkbg-aws-box/'+ object.key #=> no data is fetched from s3, just a list of keys
+      g = Graffiti.new('image_url' => url)
+    end
+
+
+  end
+
 end
