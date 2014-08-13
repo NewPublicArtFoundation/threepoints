098439ab22271fd6723a54bbbf03c5c106bd243f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-08-13 18:07:48 -0400

    Save the year dates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,8 +36,8 @@ setTimeout(function(){
         $('.home-hero-wrap').addClass('appear-timeline');
         setTimeout(function(){
           $('.home-hero-wrap').addClass('appear-settime');
-          $(".number-start").animateNumbers(430, true, 1000)
-          $(".number-end").animateNumbers(770, true, 1000)
+          $(".number-start-year").animateNumbers(2000, true, 1000);
+          $(".number-end-year").animateNumbers(2008, true, 1000);
           setTimeout(function(){
             $('.home-hero-wrap').addClass('appear-final');
           }, 2000)
