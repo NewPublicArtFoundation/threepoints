babe09cc4a2e42e32798285554452bbf63bca5e6
RememberLenny <leonard_bogdonoff@condenast.com>
2014-08-18 23:53:26 -0400

    Conditionally load all the javascripts

--- app/assets/javascripts/mapboxHome.js
+++ app/assets/javascripts/mapboxHome.js
@@ -1,44 +1,46 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoibGtiZ2lmdCIsImEiOiJyTERaTml3In0.y2wd_Fi58ux53Wm1Ur1qCg';
-var map = L.mapbox.map('map', 'examples.map-h67hf2ic', {
-  zoomControl: false
-}).setView([geoJSON[0].geometry.coordinates[1], geoJSON[0].geometry.coordinates[0]], 12);
+if($('#map').length > 0){
+  L.mapbox.accessToken = 'pk.eyJ1IjoibGtiZ2lmdCIsImEiOiJyTERaTml3In0.y2wd_Fi58ux53Wm1Ur1qCg';
+  var map = L.mapbox.map('map', 'examples.map-h67hf2ic', {
+    zoomControl: false
+  }).setView([geoJSON[0].geometry.coordinates[1], geoJSON[0].geometry.coordinates[0]], 12);
 
-// Disable drag and zoom handlers.
-map.dragging.disable();
-map.touchZoom.disable();
-map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
+  // Disable drag and zoom handlers.
+  map.dragging.disable();
+  map.touchZoom.disable();
+  map.doubleClickZoom.disable();
+  map.scrollWheelZoom.disable();
 
-// Disable tap handler, if present.
-if (map.tap) map.tap.disable();
+  // Disable tap handler, if present.
+  if (map.tap) map.tap.disable();
 
-map.featureLayer.setGeoJSON(geoJSON);
+  map.featureLayer.setGeoJSON(geoJSON);
 
-  // var markerList = document.getElementById('marker-list');
-  // map.featureLayer.on('ready', function(e) {
-  //     map.featureLayer.eachLayer(function(layer) {
-  //         var item = markerList.appendChild(document.createElement('li'));
-  //         item.innerHTML = layer.toGeoJSON().properties.title;
-  //         item.onclick = function() {
-  //            map.setView(layer.getLatLng(), 14);
-  //            layer.openPopup();
-  //         };
-  //     });
-  // });
-
-
-  // for( var i = 0; i < geoJSON.length; i++ ){
-  //   var lon = geoJSON[i].geometry.coordinates[0],
-  //       lat  = geoJSON[i].geometry.coordinates[1];
-  // }
+    // var markerList = document.getElementById('marker-list');
+    // map.featureLayer.on('ready', function(e) {
+    //     map.featureLayer.eachLayer(function(layer) {
+    //         var item = markerList.appendChild(document.createElement('li'));
+    //         item.innerHTML = layer.toGeoJSON().properties.title;
+    //         item.onclick = function() {
+    //            map.setView(layer.getLatLng(), 14);
+    //            layer.openPopup();
+    //         };
+    //     });
+    // });
 
 
-$(window).scroll(function(){
-  var isMapTop = $(window).scrollTop() >= $('#map').offset().top - 25  && $('#map').offset().top + 25 >= $(window).scrollTop();
-  var isMapBottom = $(window).scrollTop() <= $('#map').offset().top + $('#map').height();
-  if(isMapTop){
-    $('body').addClass('map-inview');
-  } else {
-    $('body').removeClass('map-inview');
-  }
-});
+    // for( var i = 0; i < geoJSON.length; i++ ){
+    //   var lon = geoJSON[i].geometry.coordinates[0],
+    //       lat  = geoJSON[i].geometry.coordinates[1];
+    // }
+
+
+  $(window).scroll(function(){
+    var isMapTop = $(window).scrollTop() >= $('#map').offset().top - 25  && $('#map').offset().top + 25 >= $(window).scrollTop();
+    var isMapBottom = $(window).scrollTop() <= $('#map').offset().top + $('#map').height();
+    if(isMapTop){
+      $('body').addClass('map-inview');
+    } else {
+      $('body').removeClass('map-inview');
+    }
+  });
+}
--- app/assets/javascripts/timelineDeclaration.js
+++ app/assets/javascripts/timelineDeclaration.js
@@ -1,107 +1,110 @@
-var eventsJSON = {
-    "timeline":
-    {
-        "headline":"Lame in Shanghai",
-        "type":"default",
-        "text":"<p>Before and after</p>",
-        "asset": {
-            "media":"https://farm9.staticflickr.com/8011/7659718044_d93886d294_z_d.jpg",
-            "credit":"Gift",
-            "caption":"Abandoned Parking Structure, Shanghai, China"
-        },
-        "date": [
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Rooftop in the middle of downtown Shanghai, China",
-                "text":"<p>Blank walls right next to the freeway</p>",
-                "tag":"blankwalls",
-                "classname":"optionaluniqueclassnamecanbeaddedhere",
-                "asset": {
-                    "media":"http://i.imgur.com/LurRBXE.jpg",
-                    "thumbnail":"http://i.imgur.com/LurRBXEs.jpg",
-                    "credit":"Gift",
-                    "caption":"Blank walls to be painted"
-                }
-            },
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Lame in action",
-                "text":"<p>Starting off the piece with a sketch</p>",
-                "tag":"graffiti",
-                "classname":"optionaluniqueclassnamecanbeaddedhere",
-                "asset": {
-                    "media":"https://farm8.staticflickr.com/7111/7668246566_3dcd80c505_z_d.jpg",
-                    "thumbnail":"https://farm8.staticflickr.com/7111/7668246566_3dcd80c505_q_d.jpg",
-                    "credit":"Gift",
-                    "caption":"Birds eye Lame"
-                }
-            },
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Sketching",
-                "text":"<p>Lame plans out his piece</p>",
-                "tag":"sketching graffiti",
-                "classname":"optionaluniqueclassnamecanbeaddedhere",
-                "asset": {
-                    "media":"https://farm9.staticflickr.com/8023/7668245710_d3708541d7_z_d.jpg",
-                    "thumbnail":"https://farm9.staticflickr.com/8023/7668245710_d3708541d7_q_d.jpg",
-                    "credit":"Gift",
-                    "caption":"Sketching"
-                }
-            },
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Lame looking badass",
-                "text":"<p>No words needed</p>",
-                "tag":"posing",
-                "classname":"optionaluniqueclassnamecanbeaddedhere",
-                "asset": {
-                    "media":"https://farm9.staticflickr.com/8282/7668243546_3b7fd32f42_z_d.jpg",
-                    "thumbnail":"https://farm9.staticflickr.com/8282/7668243546_3b7fd32f42_q_d.jpg",
-                    "credit":"Gift",
-                    "caption":"Contemplating"
-                }
-            },
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Final piece",
-                "text":"<p>Skull character</p>",
-                "tag":"graffiti character",
-                "classname":"optionaluniqueclassnamecanbeaddedhere",
-                "asset": {
-                    "media":"https://farm9.staticflickr.com/8294/7668242622_8ccf48d6b7_z_d.jpg",
-                    "thumbnail":"https://farm9.staticflickr.com/8294/7668242622_8ccf48d6b7_q_d.jpg",
-                    "credit":"Gift",
-                    "caption":"Skull character"
-                }
-            }
-        ],
-        "era": [
-            {
-                "startDate":"2012,7,29",
-                "endDate":"2012,7,29",
-                "headline":"Headline Goes Here",
-                "text":"<p>Body text goes here, some HTML is OK</p>",
-                "tag":"This is Optional"
-            }
+if($('#timeline-embed').length > 0  ){
 
-        ]
+    var eventsJSON = {
+        "timeline":
+        {
+            "headline":"Lame in Shanghai",
+            "type":"default",
+            "text":"<p>Before and after</p>",
+            "asset": {
+                "media":"https://farm9.staticflickr.com/8011/7659718044_d93886d294_z_d.jpg",
+                "credit":"Gift",
+                "caption":"Abandoned Parking Structure, Shanghai, China"
+            },
+            "date": [
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Rooftop in the middle of downtown Shanghai, China",
+                    "text":"<p>Blank walls right next to the freeway</p>",
+                    "tag":"blankwalls",
+                    "classname":"optionaluniqueclassnamecanbeaddedhere",
+                    "asset": {
+                        "media":"http://i.imgur.com/LurRBXE.jpg",
+                        "thumbnail":"http://i.imgur.com/LurRBXEs.jpg",
+                        "credit":"Gift",
+                        "caption":"Blank walls to be painted"
+                    }
+                },
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Lame in action",
+                    "text":"<p>Starting off the piece with a sketch</p>",
+                    "tag":"graffiti",
+                    "classname":"optionaluniqueclassnamecanbeaddedhere",
+                    "asset": {
+                        "media":"https://farm8.staticflickr.com/7111/7668246566_3dcd80c505_z_d.jpg",
+                        "thumbnail":"https://farm8.staticflickr.com/7111/7668246566_3dcd80c505_q_d.jpg",
+                        "credit":"Gift",
+                        "caption":"Birds eye Lame"
+                    }
+                },
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Sketching",
+                    "text":"<p>Lame plans out his piece</p>",
+                    "tag":"sketching graffiti",
+                    "classname":"optionaluniqueclassnamecanbeaddedhere",
+                    "asset": {
+                        "media":"https://farm9.staticflickr.com/8023/7668245710_d3708541d7_z_d.jpg",
+                        "thumbnail":"https://farm9.staticflickr.com/8023/7668245710_d3708541d7_q_d.jpg",
+                        "credit":"Gift",
+                        "caption":"Sketching"
+                    }
+                },
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Lame looking badass",
+                    "text":"<p>No words needed</p>",
+                    "tag":"posing",
+                    "classname":"optionaluniqueclassnamecanbeaddedhere",
+                    "asset": {
+                        "media":"https://farm9.staticflickr.com/8282/7668243546_3b7fd32f42_z_d.jpg",
+                        "thumbnail":"https://farm9.staticflickr.com/8282/7668243546_3b7fd32f42_q_d.jpg",
+                        "credit":"Gift",
+                        "caption":"Contemplating"
+                    }
+                },
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Final piece",
+                    "text":"<p>Skull character</p>",
+                    "tag":"graffiti character",
+                    "classname":"optionaluniqueclassnamecanbeaddedhere",
+                    "asset": {
+                        "media":"https://farm9.staticflickr.com/8294/7668242622_8ccf48d6b7_z_d.jpg",
+                        "thumbnail":"https://farm9.staticflickr.com/8294/7668242622_8ccf48d6b7_q_d.jpg",
+                        "credit":"Gift",
+                        "caption":"Skull character"
+                    }
+                }
+            ],
+            "era": [
+                {
+                    "startDate":"2012,7,29",
+                    "endDate":"2012,7,29",
+                    "headline":"Headline Goes Here",
+                    "text":"<p>Body text goes here, some HTML is OK</p>",
+                    "tag":"This is Optional"
+                }
+
+            ]
+        }
     }
-}
 
-var timelineConfig = {
-  type: 'timeline',
-  width: '100%',
-  height: '500',
-  source: eventsJSON, //get the events.json format from https://github.com/VeriteCo/TimelineJS#file-formats
-  embed_id: 'time_line'
-}
+    var timelineConfig = {
+      type: 'timeline',
+      width: '100%',
+      height: '500',
+      source: eventsJSON, //get the events.json format from https://github.com/VeriteCo/TimelineJS#file-formats
+      embed_id: 'time_line'
+    }
 
-$(document).ready(function() {
-  createStoryJS(timelineConfig);
-});
+    $(document).ready(function() {
+      createStoryJS(timelineConfig);
+    });
+}
