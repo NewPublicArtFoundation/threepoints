761bae49eb11b9417f715d20a27fd00b33bedb2f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-09-03 13:40:39 -0400

    Save the protect from forgery change

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,4 +1,5 @@
 class InstagramController < ApplicationController
+  protect_from_forgery except: :create
 
   def subscription
     client = Instagram.client(:access_token => session[:access_token])
@@ -10,10 +11,6 @@ class InstagramController < ApplicationController
   end
 
   def create
-    protect_from_forgery with: :null_session,
-    if: Proc.new {
-      |c| c.request.format == 'application/json'
-    }
 
     request.body.rewind  # in case someone already read it
     data = request.body.read
