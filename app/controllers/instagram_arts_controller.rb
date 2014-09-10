a8dc8c933fb32b922117feb8260ae949bb1865d7
RememberLenny <leonard_bogdonoff@condenast.com>
2014-09-10 09:12:38 -0400

    Create a limit on the number instagram art pieces made

--- app/controllers/instagram_arts_controller.rb
+++ app/controllers/instagram_arts_controller.rb
@@ -4,7 +4,7 @@ class InstagramArtsController < InheritedResources::Base
   # GET /arts
   # GET /arts.json
   def index
-    @instagram_arts = InstagramArt.all
+    @instagram_arts = InstagramArt.limit(20).all
   end
 
   # GET /arts/1
