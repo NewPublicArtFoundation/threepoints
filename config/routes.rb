ec6bd935296b0a19cf8d5bfe5b6f910c514e4f35
RememberLenny <leonard_bogdonoff@condenast.com>
2014-09-11 10:41:11 -0400

    Create the new routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :instagram_arts
-  resources :instagram_arts, as: 'a'
+  get 'a/:id', to: 'instagram_arts#show', as: :a
 
   resources :arts do
     resources :uploads
