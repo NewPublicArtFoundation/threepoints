e31ee7e2c9c40ff2814d71291f5ade5af49509d2
RememberLenny <leonard_bogdonoff@condenast.com>
2014-09-11 11:19:38 -0400

    SAve default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
      truncate(c, :length => 140)
    end
 
+    def default_meta_tags
+      {
+        :title       => 'Public Artfound',
+        :description => 'The only platform to showcase street art and graffiti.',
+        :keywords    => 'graffiti, street art, urban, photography, street photography, hiphop, art, streetart, gallery, style',
+        :separator   => "&mdash;".html_safe,
+      }
+    end
+
 end
